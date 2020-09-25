f0d44e8ae1fb265ad94cbcc046900ce9a5783278
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-09-25 09:52:26 -0500

    routes and search components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header.jsx';
 
 const items = [
   {
@@ -16,10 +21,43 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: 'The color red',
+    value: 'red',
+  },
+  {
+    label: 'The color green',
+    value: 'green',
+  },
+  {
+    label: 'A shade of blue',
+    value: 'blue',
+  },
+];
+
 export default () => {
+  const [selected, setSelected] = useState(0);
   return (
     <div>
-      <Accordion items={items} />
+      <Header />
+      <Route path='/'>
+        <Accordion items={items} />
+      </Route>
+      <Route path='/list'>
+        <Search />
+      </Route>
+      <Route path='/dropdown'>
+        <Dropdown
+          label='Select a color'
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path='/translate'>
+        <Translate />
+      </Route>
     </div>
   );
 };
